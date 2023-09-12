c607115471e0cc2910d341626e88d78962dd067e
unknown <mutungaeric839@gmail.com>
2023-09-12 18:34:19 +0300

    boolean and Char

--- home.kt
+++ home.kt
@@ -1,10 +1,20 @@
 fun main() {
-    val floatValue: Float = 3.14159265f
-    val doubleValue: Double = 3.141592653589793
+    // Char examples  used to store single characters, such as letters, digits, symbols, or any other character from the Unicode character set. You can use single quotes (') to declare a Char value. For example, 'A', '1', '%', and '✓' are valid Char values.
+    val firstLetter: Char = 'A'
+    val digit: Char = '7'
+    val symbol: Char = '$'
 
-    println("Float Value: $floatValue")
-    println("Double Value: $doubleValue")
+    // Boolean examples
+    val isTrue: Boolean = true
+    val isFalse: Boolean = false
 
-    val sum = floatValue + doubleValue
-    println("Sum: $sum")
+    // Printing values and their types
+    println("Char examples:")
+    println("First Letter: $firstLetter is a Char")
+    println("Digit: $digit is a Char")
+    println("Symbol: $symbol is a Char")
+
+    println("\nBoolean examples:")
+    println("Is True? $isTrue is a Boolean")
+    println("Is False? $isFalse is a Boolean")
 }
