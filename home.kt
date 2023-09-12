16d5f5f4b88050845e72be5be907f5994d522d53
unknown <mutungaeric839@gmail.com>
2023-09-13 01:48:00 +0300

    if,elseif,else

--- home.kt
+++ home.kt
@@ -1,15 +1,11 @@
 fun main() {
-    var x = 5
-val result = x++
-// result is 5, and x is now 6
-println(result) //5
-println(x)//6
+    val num = 42
 
-var y = 5
-val result2 = ++y
-// result is 6, and x is now 6
-
-println(result2) //6
-println(y)//6
-
+    if (num > 50) {
+        println("Number is greater than 50")
+    } else if (num == 50) {
+        println("Number is exactly 50")
+    } else {
+        println("Number is less than 50")
+    }
 }
