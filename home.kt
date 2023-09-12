91a81ed35a01fd09b0a376e9d729c3ee3f1bc4f9
unknown <mutungaeric839@gmail.com>
2023-09-12 18:54:56 +0300

    post increment

--- home.kt
+++ home.kt
@@ -1,20 +1,8 @@
 fun main() {
-    // Char examples  used to store single characters, such as letters, digits, symbols, or any other character from the Unicode character set. You can use single quotes (') to declare a Char value. For example, 'A', '1', '%', and '✓' are valid Char values.
-    val firstLetter: Char = 'A'
-    val digit: Char = '7'
-    val symbol: Char = '$'
+    var x = 5
+val result = x++
+// result is 5, and x is now 6
+println(result) //5
+println(x)//6
 
-    // Boolean examples
-    val isTrue: Boolean = true
-    val isFalse: Boolean = false
-
-    // Printing values and their types
-    println("Char examples:")
-    println("First Letter: $firstLetter is a Char")
-    println("Digit: $digit is a Char")
-    println("Symbol: $symbol is a Char")
-
-    println("\nBoolean examples:")
-    println("Is True? $isTrue is a Boolean")
-    println("Is False? $isFalse is a Boolean")
 }
