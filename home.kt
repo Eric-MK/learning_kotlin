ef3c6556164fd4b7c528879572b24ebe7c46287b
unknown <mutungaeric839@gmail.com>
2023-09-13 20:15:50 +0300

    while loop

--- home.kt
+++ home.kt
@@ -1,19 +1,8 @@
 fun main() {
-/*     for (i in 1..5) {
+    var i = 1
+    while (i <= 5) {
         println(i)
-    }
-//until keyword is used to create a range that includes the starting value but excludes the ending value. 
-    for (i in 1 until 6) {
-        println(i)
-    } */
-    
-    for (i in 10 downTo(1)) {
-        println(i)
-
-    }
-
-    for (i in 1..10 step 2) {
-        println(i)
+        i++
     }
     
 }
