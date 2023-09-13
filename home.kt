670d1be8d74b2c1a6d0db9b7e8f8cb65d46cd74a
unknown <mutungaeric839@gmail.com>
2023-09-13 07:24:33 +0300

    use of vararg key word

--- home.kt
+++ home.kt
@@ -1,3 +1,5 @@
+//the vararg keyword is used to define a variable number of arguments (varargs) in a function. This allows you to pass a variable number of arguments of the same type to a function. When you use vararg, the arguments are treated as an array within the function, making it flexible for functions that need to accept multiple values.
+
 fun exampleFunction(vararg values: Int) {
     for (value in values) {
         println(value)
