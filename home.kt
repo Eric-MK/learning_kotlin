536d70bded7c0fc1569cd17f29dfedd607023951
unknown <mutungaeric839@gmail.com>
2023-09-13 07:35:58 +0300

    for loop using until keyword

--- home.kt
+++ home.kt
@@ -2,6 +2,11 @@ fun main() {
     for (i in 1..5) {
         println(i)
     }
+//until keyword is used to create a range that includes the starting value but excludes the ending value. 
+    for (i in 1 until 6) {
+        println(i)
+    }
+    
 }
 
 
