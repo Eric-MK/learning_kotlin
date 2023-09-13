bdb6c5400c29de586c98c88cac3e914d72294311
unknown <mutungaeric839@gmail.com>
2023-09-13 07:38:44 +0300

    for print backwards using downTo keyword

--- home.kt
+++ home.kt
@@ -1,12 +1,16 @@
 fun main() {
-    for (i in 1..5) {
+/*     for (i in 1..5) {
         println(i)
     }
 //until keyword is used to create a range that includes the starting value but excludes the ending value. 
     for (i in 1 until 6) {
         println(i)
-    }
+    } */
     
+    for (i in 10 downTo(1)) {
+        println(i)
+
+    }
 }
 
 
