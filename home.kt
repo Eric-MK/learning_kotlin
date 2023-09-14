c7144dad4cbe18d8ee3efa06ae0f0c91caed8f29
unknown <mutungaeric839@gmail.com>
2023-09-14 05:30:25 +0300

    nested while loop

--- home.kt
+++ home.kt
@@ -1,19 +1,20 @@
 fun main() {
-    var number = 0
-    while (number < 10) {
-        number++
-        
+    var outer = 1  // Initialize the outer loop counter
 
-if(number == 7)
-{
+    // Start the outer loop
+    while (outer <= 3) {  // Continue as long as outer is less than or equal to 3
+        var inner = 1  // Initialize the inner loop counter
 
-    continue
-}
-        println(number)
-        
+        // Start the inner loop
+        while (inner <= 3) {  // Continue as long as inner is less than or equal to 3
+            // Print a message that includes the current values of outer and inner
+            println("Outer: $outer, Inner: $inner")
+
+            inner++  // Increment the inner loop counter
+        }
+        // End of the inner loop
+
+        outer++  // Increment the outer loop counter
     }
-    
+    // End of the outer loop
 }
-
-
-
