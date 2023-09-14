14ecc80b20fa6093e5edc622b0f7c8ee9c624696
unknown <mutungaeric839@gmail.com>
2023-09-14 05:47:47 +0300

    loop label

--- home.kt
+++ home.kt
@@ -1,20 +1,10 @@
-fun main() {
-    var outer = 1  // Initialize the outer loop counter
-
-    // Start the outer loop
-    while (outer <= 3) {  // Continue as long as outer is less than or equal to 3
-        var inner = 1  // Initialize the inner loop counter
-
-        // Start the inner loop
-        while (inner <= 3) {  // Continue as long as inner is less than or equal to 3
-            // Print a message that includes the current values of outer and inner
-            println("Outer: $outer, Inner: $inner")
-
-            inner++  // Increment the inner loop counter
+// Define an outer loop labeled as "loop"
+loop@ for (i in 1..3) {
+    println("Outer loop: $i")  // Print the value of i for the outer loop
+    for (j in 1..3) {
+        if (i == 2 && j == 2) {
+            break@loop // Breaks out of the outer loop labeled as "loop"
         }
-        // End of the inner loop
-
-        outer++  // Increment the outer loop counter
+        println("Inner loop: $j")  // Print the value of j for the inner loop
     }
-    // End of the outer loop
 }
