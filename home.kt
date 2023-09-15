224b368b22713e1d56beea2a8fe83242c267078b
unknown <mutungaeric839@gmail.com>
2023-09-15 05:41:09 +0300

    returns maximum and minimum

--- home.kt
+++ home.kt
@@ -1,13 +1,13 @@
 fun main() {
-    val numbers = arrayOf(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5)
+val numbers = arrayOf(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5)
 
-    // Find the maximum and minimum values in the array
-    val maxNumber = numbers.maxOrNull()
-    val minNumber = numbers.minOrNull()
-    
-    // Print the results
-    println("Maximum value: $maxNumber")
-    println("Minimum value: $minNumber")
+// Find the maximum and minimum values in the array
+val maxNumber = numbers.maxOrNull()
+val minNumber = numbers.minOrNull()
+
+// Print the results
+println("Maximum value: $maxNumber")
+println("Minimum value: $minNumber")
 }
 
 
