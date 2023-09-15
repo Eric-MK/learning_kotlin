b819bf08cb4b59f351ca670d036f1e39303107d5
unknown <mutungaeric839@gmail.com>
2023-09-15 05:32:56 +0300

    returns maximum and minimum

--- home.kt
+++ home.kt
@@ -1,20 +1,13 @@
 fun main() {
-/*     Array: This represents a fixed-size array where the size is defined at the time of creation and cannot be changed.
- */// Define an array of integers with a size of 5
-val numbers = arrayOf(1, 2, 3, 4, 5, "name")
+    val numbers = arrayOf(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5)
 
-// Access elements by index
-val firstNumber = numbers[0]  // Access the first element (index 0)
-val secondNumber = numbers[1] // Access the second element (index 1)
-
-// Get the size of the array
-val size = numbers.size
-
-// Loop through the array
-for (number in numbers) {
-    println(number)
-}
-
+    // Find the maximum and minimum values in the array
+    val maxNumber = numbers.maxOrNull()
+    val minNumber = numbers.minOrNull()
+    
+    // Print the results
+    println("Maximum value: $maxNumber")
+    println("Minimum value: $minNumber")
 }
 
 
