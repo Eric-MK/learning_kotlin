c6e9d4f89ca087c34dda22d7fa1b53db9901cbf5
unknown <mutungaeric839@gmail.com>
2023-09-17 13:51:20 +0300

    primary constructor

--- home.kt
+++ home.kt
@@ -1,14 +1,15 @@
 fun main() {
-val numbers = arrayOf(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5)
-
-// Find the maximum and minimum values in the array
-val maxNumber = numbers.maxOrNull()
-val minNumber = numbers.minOrNull()
-
-// Print the results
-println("Maximum value: $maxNumber")
-println("Minimum value: $minNumber")
+    val person = Person("John", "Doe")
+    println(person.fullName) // Prints "John Doe"
+    
 }
 
+class Person(firstName: String, lastName: String) {
+    val fullName: String
+
+    init {
+        fullName = "$firstName $lastName"
+    }
+}
 
 
