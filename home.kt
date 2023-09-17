4e3a67a2b6210dcebb9a4ebdd27d44c787f5c8bf
unknown <mutungaeric839@gmail.com>
2023-09-17 18:00:34 +0300

    OOP: Companion Object

--- home.kt
+++ home.kt
@@ -1,32 +1,15 @@
-class MyClass {
-    // Declare a lateinit property
-    lateinit var name: String
-
-    // Function to initialize the property
-    fun initializeName() {
-        name = "John"
-    }
-
-    // Function to use the property
-    fun printName() {
-        if (this::name.isInitialized) {
-            println("Name: $name")
-        } else {
-            println("Name is not initialized yet")
-        }
+class Person(val name: String) {
+    companion object {
+        const val DEFAULT_NAME = "Unknown" // A constant property
+        fun createDefaultPerson() = Person(DEFAULT_NAME) // A factory method
     }
 }
 
 fun main() {
-    val myObject = MyClass()
+    // Accessing a constant property from the companion object
+    println("Default Name: ${Person.DEFAULT_NAME}")
 
-    // Accessing the property before initialization would result in an exception
-    // Uncommenting the next line would result in a 'lateinit' property access exception
-    // println(myObject.name)
-
-    // Initialize the property
-    myObject.initializeName()
-
-    // Access and print the property
-    myObject.printName()
+    // Using a factory method from the companion object to create a Person instance
+    val defaultPerson = Person.createDefaultPerson()
+    println("Default Person Name: ${defaultPerson.name}")
 }
