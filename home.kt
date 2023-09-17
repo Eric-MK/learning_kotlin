0e753b40089cb05c6e5168f1906e33fb7c144f38
unknown <mutungaeric839@gmail.com>
2023-09-17 17:48:17 +0300

    lateinit

--- home.kt
+++ home.kt
@@ -1,27 +1,32 @@
-class Person {
-    // Declare an age property with an initial value of 0
-    var age: Int = 0
-        // Custom getter
-        get() {
-            println("Getting age")
-            return field // 'field' refers to the actual property value
+class MyClass {
+    // Declare a lateinit property
+    lateinit var name: String
+
+    // Function to initialize the property
+    fun initializeName() {
+        name = "John"
+    }
+
+    // Function to use the property
+    fun printName() {
+        if (this::name.isInitialized) {
+            println("Name: $name")
+        } else {
+            println("Name is not initialized yet")
         }
-        // Custom setter
-        set(value) {
-            println("Setting age to $value")
-            field = value // 'field' allows you to assign the value to the property
-        }
+    }
 }
 
 fun main() {
-    // Create an instance of the Person class
-    val person = Person()
-    
-    // Access the age property (calls custom getter)
-    val currentAge = person.age
-    println("Current age: $currentAge") // Output: "Getting age\nCurrent age: 0"
-    
-    // Modify the age property (calls custom setter)
-    person.age = 30
-    println("Updated age: ${person.age}") // Output: "Setting age to 30\nGetting age\nUpdated age: 30"
+    val myObject = MyClass()
+
+    // Accessing the property before initialization would result in an exception
+    // Uncommenting the next line would result in a 'lateinit' property access exception
+    // println(myObject.name)
+
+    // Initialize the property
+    myObject.initializeName()
+
+    // Access and print the property
+    myObject.printName()
 }
