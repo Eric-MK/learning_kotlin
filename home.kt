80a01b1f9e085d008f6062b7ce6d7504f2b2a683
unknown <mutungaeric839@gmail.com>
2023-09-17 17:11:24 +0300

    initializer blocks

--- home.kt
+++ home.kt
@@ -1,15 +1,26 @@
-fun main() {
-    val person = Person("John", "Doe")
-    println(person.fullName) // Prints "John Doe"
-    
-}
-
-class Person(firstName: String, lastName: String) {
-    val fullName: String
+class ExampleClass(param1: String, param2: Int) {
+    val property1: String
+    val property2: Int
 
     init {
-        fullName = "$firstName $lastName"
+        println("First init block (property1) executed")
+        property1 = param1.toUpperCase()
+    }
+
+    init {
+        println("Second init block (property2) executed")
+        property2 = param2 * 2
+    }
+
+    companion object {
+        init {
+            println("Companion object init block executed")
+        }
     }
 }
 
-
+fun main() {
+    val example = ExampleClass("hello", 3)
+    println("Property 1: ${example.property1}")
+    println("Property 2: ${example.property2}")
+}
