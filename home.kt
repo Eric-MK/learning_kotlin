5b2c67e976b498004f593a74e2e5f09a491f9bd4
unknown <mutungaeric839@gmail.com>
2023-09-17 17:42:55 +0300

    getter and setters

--- home.kt
+++ home.kt
@@ -1,19 +1,27 @@
-class Person(
-    val firstName: String = "John",
-    val lastName: String = "Doe",
-    val age: Int = 30
-) {
-    val fullName = "$firstName $lastName"
+class Person {
+    // Declare an age property with an initial value of 0
+    var age: Int = 0
+        // Custom getter
+        get() {
+            println("Getting age")
+            return field // 'field' refers to the actual property value
+        }
+        // Custom setter
+        set(value) {
+            println("Setting age to $value")
+            field = value // 'field' allows you to assign the value to the property
+        }
 }
 
 fun main() {
-    // Creating instances with different sets of parameters
-    val person1 = Person()
-    val person2 = Person("Alice")
-    val person3 = Person("Bob", "Smith", 25)
-
-    // Printing the properties
-    println(person1.fullName) // Output: "John Doe"
-    println(person2.fullName) // Output: "Alice Doe"
-    println(person3.fullName) // Output: "Bob Smith"
+    // Create an instance of the Person class
+    val person = Person()
+    
+    // Access the age property (calls custom getter)
+    val currentAge = person.age
+    println("Current age: $currentAge") // Output: "Getting age\nCurrent age: 0"
+    
+    // Modify the age property (calls custom setter)
+    person.age = 30
+    println("Updated age: ${person.age}") // Output: "Setting age to 30\nGetting age\nUpdated age: 30"
 }
