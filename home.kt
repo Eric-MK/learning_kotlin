810e1d8647e66999fd3c6da29457cb9433350233
unknown <mutungaeric839@gmail.com>
2023-09-25 20:59:20 +0300

    data class

--- home.kt
+++ home.kt
@@ -1,24 +1,26 @@
-// Define an abstract class called 'Shape'
-abstract class Shape {
-    // Abstract method to calculate area (no implementation)
-    abstract fun calculateArea(): Double
+// Define a data class called 'Person' with properties
+data class Person(val name: String, val age: Int)
 
-    // Concrete method to display information
-    fun displayInfo() {
-        println("This is a shape.")
-    }
-}
+fun main() {
+    // Create instances of the 'Person' data class
+    val person1 = Person("Alice", 30)
+    val person2 = Person("Bob", 25)
 
-// Define a subclass 'Circle' that inherits from 'Shape'
-class Circle(private val radius: Double) : Shape() {
-    override fun calculateArea(): Double {
-        return 3.14 * radius * radius
-    }
-}
+    // Automatically generated methods:
+    // - equals() compares objects based on property values
+    println("Are person1 and person2 equal? ${person1 == person2}")
 
-// Define another subclass 'Rectangle' that inherits from 'Shape'
-class Rectangle(private val width: Double, private val height: Double) : Shape() {
-    override fun calculateArea(): Double {
-        return width * height
-    }
+    // - hashCode() generates a hash code based on property values
+    println("Hash code of person1: ${person1.hashCode()}")
+
+    // - toString() provides a string representation of the object
+    println("Person1: $person1")
+
+    // - component functions allow destructuring
+    val (name, age) = person1
+    println("Destructured: Name=$name, Age=$age")
+
+    // Create a copy of 'person1' with a modified age
+    val olderPerson = person1.copy(age = 35)
+    println("Older person: $olderPerson")
 }
