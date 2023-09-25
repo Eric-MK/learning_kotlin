22066357ec894824da6d41db2f42e8bd905ccbe5
unknown <mutungaeric839@gmail.com>
2023-09-25 19:43:48 +0300

    lazy initiallization

--- home.kt
+++ home.kt
@@ -1,33 +1,26 @@
-// Define a Singleton class called MySingleton
-object MySingleton {
-    // Private nullable variable to hold the Singleton instance
-    private var instance: MySingleton? = null
-
-    // Function to get the Singleton instance
-    fun getInstance(): MySingleton {
-        // If the instance is not created, create it
-        if (instance == null) {
-            instance = MySingleton
-        }
-        // Return the Singleton instance
-        return instance!!
+class ExpensiveObject {
+    init {
+        println("ExpensiveObject created")
     }
 
-    // Function to demonstrate a method of the Singleton
     fun doSomething() {
-        println("Singleton is doing something")
+        println("ExpensiveObject is doing something")
     }
 }
 
-// Entry point of the program
 fun main() {
-    // Get the Singleton instance using the getInstance() method
-    val singleton1 = MySingleton.getInstance()
-    val singleton2 = MySingleton.getInstance()
+    // Declare a lazy property of type ExpensiveObject
+    val lazyObject: ExpensiveObject by lazy {
+        ExpensiveObject()
+    }
 
-    // Check if both references point to the same instance
-    println(singleton1 === singleton2) // Output: true (both references are the same)
+    println("Before accessing lazyObject")
 
-    // Call a method of the Singleton
-    singleton1.doSomething()
+    // Accessing lazyObject for the first time (initialization occurs here)
+    lazyObject.doSomething()
+
+    println("After accessing lazyObject")
+
+    // Accessing lazyObject again (no reinitialization occurs)
+    lazyObject.doSomething()
 }
