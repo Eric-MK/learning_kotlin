8475eeaa0dc4650ff45a855975c098386953045a
unknown <mutungaeric839@gmail.com>
2023-09-25 20:52:11 +0300

    abstract class

--- home.kt
+++ home.kt
@@ -1,17 +1,24 @@
-// Define a sealed class named 'Result'
-sealed class Result {
-    // Define a data class 'Success' as a subclass of 'Result'
-    data class Success(val data: String) : Result()
-    
-    // Define a data class 'Error' as a subclass of 'Result'
-    data class Error(val message: String) : Result()
+// Define an abstract class called 'Shape'
+abstract class Shape {
+    // Abstract method to calculate area (no implementation)
+    abstract fun calculateArea(): Double
+
+    // Concrete method to display information
+    fun displayInfo() {
+        println("This is a shape.")
+    }
 }
 
-// Function to process a 'Result' object
-fun processResult(result: Result) {
-    // Use a 'when' expression to handle different cases of 'Result'
-    when (result) {
-        is Result.Success -> println("Success: ${result.data}")
-        is Result.Error -> println("Error: ${result.message}")
+// Define a subclass 'Circle' that inherits from 'Shape'
+class Circle(private val radius: Double) : Shape() {
+    override fun calculateArea(): Double {
+        return 3.14 * radius * radius
+    }
+}
+
+// Define another subclass 'Rectangle' that inherits from 'Shape'
+class Rectangle(private val width: Double, private val height: Double) : Shape() {
+    override fun calculateArea(): Double {
+        return width * height
     }
 }
