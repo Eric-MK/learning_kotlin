dae8c9ee95ad233e8c08562e4388e76285cb7de3
unknown <mutungaeric839@gmail.com>
2023-09-26 04:10:10 +0300

    Zipping and Association

--- home.kt
+++ home.kt
@@ -1,33 +1,13 @@
 fun main() {
-    val numbers = listOf(1, 2, 3, 4, 5)
+    // Zipping two lists
+    val list1 = listOf("A", "B", "C")
+    val list2 = listOf(1, 2, 3)
+    val zipped = list1.zip(list2)
+    println("Zipped: $zipped")
 
-    // Example 1: Squaring each number
-    val squaredNumbers = numbers.map { it * it }
-
-    // Example 2: Doubling each number
-    val doubledNumbers = numbers.map { it * 2 }
-
-    // Example 3: Converting numbers to strings
-    val numberStrings = numbers.map { it.toString() }
-
-    // Example 4: Mapping to custom objects
-    data class Person(val name: String, val age: Int)
-    val people = numbers.map { Person("Person-$it", it * 10) }
-
-    // Example 5: Applying a more complex transformation
-    val customMapping = numbers.map {
-        if (it % 2 == 0) {
-            "Even"
-        } else {
-            "Odd"
-        }
-    }
-
-    // Printing the results
-    println("Original Numbers: $numbers")
-    println("Squared Numbers: $squaredNumbers")
-    println("Doubled Numbers: $doubledNumbers")
-    println("Number Strings: $numberStrings")
-    println("People: $people")
-    println("Custom Mapping: $customMapping")
+    // Associating elements from two lists
+    val keys = listOf("A", "B", "C")
+    val values = listOf(1, 2, 3)
+    val association = keys.associateWith { values[keys.indexOf(it)] }
+    println("Association: $association")
 }
