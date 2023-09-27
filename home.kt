842487de1afa50154b20f0d9f4b5ccf3b8bd489a
unknown <mutungaeric839@gmail.com>
2023-09-27 18:40:08 +0300

    Retrieve Single Elements first and last function

--- home.kt
+++ home.kt
@@ -2,8 +2,11 @@
 fun main() {
     val numbers = listOf(1, 2, 3, 4, 5)
 
-    // Find the first even number
-    val firstEven = numbers.find { it % 2 == 0 }
-    
+// Retrieve the first element
+val firstElement = numbers.first()
+
+// Retrieve the last element
+val lastElement = numbers.last()
+
     }
 
