550a17acfef956bf9d91b8231a4d1501b39f5bae
unknown <mutungaeric839@gmail.com>
2023-09-27 18:31:32 +0300

    Retrieve Collection parts slice  a lsit

--- home.kt
+++ home.kt
@@ -1,19 +1,9 @@
-data class Person(val name: String, val age: Int)
 
 fun main() {
-    val people = listOf(
-        Person("Alice", 30),
-        Person("Bob", 25),
-        Person("Charlie", 30),
-        Person("David", 25),
-        Person("Eve", 35)
-    )
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
-    // Grouping people by age
-    val groupedByAge = people.groupBy { it.age }
-
-    // Printing the groups
-    groupedByAge.forEach { (age, group) ->
-        println("Age $age: $group")
+    // Retrieve a sublist of elements at indices 2, 3, and 4
+    val sublist = numbers.slice(2..4)
+    
     }
 }
