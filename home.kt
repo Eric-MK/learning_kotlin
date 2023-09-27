f332111736fa9a9cff2d8544b4c67ebc2dc73746
unknown <mutungaeric839@gmail.com>
2023-09-27 18:34:48 +0300

    Retrieve Collection parts Retrieving Elements by Index

--- home.kt
+++ home.kt
@@ -2,13 +2,9 @@
 fun main() {
     val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
-    // Retrieve a sublist of elements at indices 2, 3, and 4
-    val sublist = numbers.slice(2..4)
+    // Retrieve the first and last elements
+    val firstElement = numbers[0]
+    val lastElement = numbers[numbers.size - 1]
     
-    val numberss = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
-
-    // Partition into even and odd numbers
-    val (evenNumbers, oddNumbers) = numberss.partition { it % 2 == 0 } //the first list contains elements that match the predicate, and the second list contains elements that do not.
-
     }
 
