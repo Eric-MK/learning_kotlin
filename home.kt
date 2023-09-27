a481b20f52a7872ceba7862cfdd6613d91995bd6
unknown <mutungaeric839@gmail.com>
2023-09-27 18:40:48 +0300

    Retrieve Single Elements using elementAt

--- home.kt
+++ home.kt
@@ -2,11 +2,8 @@
 fun main() {
     val numbers = listOf(1, 2, 3, 4, 5)
 
-// Retrieve the first element
-val firstElement = numbers.first()
-
-// Retrieve the last element
-val lastElement = numbers.last()
-
+    // Retrieve the element at index 2
+    val elementAtIndex2 = numbers.elementAt(2)
+    
     }
 
